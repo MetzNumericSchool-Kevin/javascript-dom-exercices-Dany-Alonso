32b7583df348fb09ae664c93a4b67b146ed2feb7
Dany Alonso <dany.alonso.pro@gmail.com>
2025-02-10 09:59:09 +0100

    Exercice 2

--- exercices.js
+++ exercices.js
@@ -3,4 +3,15 @@
  */
 
 const titreH1 = document.querySelector("h1");
-console.log(titreH1.textContent);
+// console.log(titreH1.textContent);
+
+/**
+ * Exercice 2 - Des informations manquent !
+ */
+
+const paragrapheDeBienvenue = document.querySelector("#description_boutique");
+const paragrapheInstruction = document.createElement("p");
+paragrapheInstruction.textContent= `Pour cela, il vous suffit de cliquer sur une des potions de la liste ci-dessous pour l'acheter.`;
+document.getElementById("description_boutique").appendChild(paragrapheInstruction);
+// console.log(paragrapheDeBienvenue.textContent);
+// console.log(paragrapheInstruction.textContent);
