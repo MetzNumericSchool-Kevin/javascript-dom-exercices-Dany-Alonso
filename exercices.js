ea82726ddc04fe8cc10a258b0b39189d44a44696
Dany Alonso <dany.alonso.pro@gmail.com>
2025-02-11 09:47:50 +0100

    Exercice 6

--- exercices.js
+++ exercices.js
@@ -41,7 +41,7 @@ function alertCallArchibald() {
  */
 
 const boitesMagiques = document.querySelectorAll("#boites_magique .boite");
-console.log(boitesMagiques);
+// console.log(boitesMagiques);
 
 document
   .querySelectorAll("#btn_change_blue , #btn_change_red , #btn_change_green")
@@ -72,3 +72,49 @@ document
       }
     })
   );
+
+/**
+ * Exercice 6 - Aventurier, voici ma boutique !
+ */
+
+const potions = [
+  {
+    nom: "Potion de soin",
+    description:
+      "Cette potion rouge vif a une odeur de fraise des bois. Un seul gorgée et vos blessures se referment comme par magie ! Effets secondaires possibles: cheveux roses pendant 24h.",
+    prix: 10,
+  },
+  {
+    nom: "Potion de sommeil",
+    description:
+      "Un liquide bleu nuit qui sent la lavande et les rêves. Une goutte et vous dormirez comme un bébé dragon ! Attention: ne pas utiliser si vous devez combattre un troll dans les prochaines 8 heures.",
+    prix: 50,
+  },
+];
+
+/**
+ * Génère et affiche des cartes de potions à partir d'un tableau d'objets.
+ *
+ * @param {Array<Object>} potions - Tableau contenant les potions.
+ * @param {string} potions[].soin - Nom ou type de soin de la potion.
+ * @param {number} potions[].prix - Prix de la potion.
+ * @param {string} potions[].description - Description de la potion.
+ */
+potions.forEach((potion) => {
+  /** @type {HTMLElement} Liste où ajouter les cartes de potions */
+  const listPotions = document.querySelector("#liste_potions");
+
+  /** @type {DocumentFragment} Clone du template contenant la structure de la potion */
+  const templatePotionClone = document
+    .querySelector("#template_potion")
+    .content.cloneNode(true);
+
+  // Remplit les informations de la potion dans le clone
+  templatePotionClone.querySelector(".nom_potion").textContent = potion.nom;
+  templatePotionClone.querySelector(".prix_potion").textContent = potion.prix;
+  templatePotionClone.querySelector(".description_potion").textContent =
+    potion.description;
+
+  // Ajoute le clone modifié dans la liste des potions
+  listPotions.appendChild(templatePotionClone);
+});
